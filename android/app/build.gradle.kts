d06b0ea8682851b6aa03e6fba905bc704d9991dc
paran <paran@asabd.org>
2026-07-22 14:02:18 +0600

    Update target SDK version to 37 to align with current requirements.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 37
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
